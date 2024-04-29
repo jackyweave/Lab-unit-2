8c8ff847c1b60550ffe0e17739f52194c97d8380
jackyweave <71942159+jackyweave@users.noreply.github.com>
2024-04-29 13:56:13 -0400

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.Metrics;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+Console.WriteLine("Welcome to Grand Circus Room Generator! :) ");
+
+Console.WriteLine("Please enter the length:");
+double length = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Please enter the width:");
+double width = Convert.ToDouble(Console.ReadLine());
+
+double area = length * width;
+double perimeter = 2 * (length + area);
+
+
+Console.WriteLine($"Area of the room: {area} ");
+Console.WriteLine($"Perimeter of the room: {perimeter} ");
+
+
+
+
+ if (area <= 250)
+{
+    Console.WriteLine("This room is a small size");
+
+}
+
+else if (area > 250 && area > 650)
+{ Console.WriteLine("The room is a medium size"); }
+
+
+else
+{
+    Console.WriteLine("The Room is Large");
+}
